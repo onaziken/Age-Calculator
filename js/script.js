f34d3c06daa8f9c0f2d13a1231f703b03e3af4c1
SkylarNG <onaziken@gmail.com>
2018-07-10 21:04:50 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+console.log("Script successfully loaded");
+var age = prompt("How old are you?");
+var days = age * 365.25;
+console.log("You are have been alive for " + days + " days");
+alert("You are have been alive for " + days + " days");
