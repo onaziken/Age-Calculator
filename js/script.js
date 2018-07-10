118505ca201ac3d0efc61ab8e410474fadd0d8fa
SkylarNG <onaziken@gmail.com>
2018-07-10 21:08:42 +0100

    Just finished the simple age calculation file.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,4 @@ var age = prompt("How old are you?");
 var days = age * 365.25;
 console.log("You are have been alive for " + days + " days");
 alert("You are have been alive for " + days + " days");
+console.log("Our work is done!");
